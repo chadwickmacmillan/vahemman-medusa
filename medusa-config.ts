22f65983587a315cd16f37ecfc17cf531a45cf13
Chadwick <hello@chadwickmacmillan.com>
2025-10-27 09:09:15 -0400

    updates

--- medusa-config.ts
+++ medusa-config.ts
@@ -73,50 +73,50 @@ module.exports = defineConfig({
         ],
       },
     },
-    // {
-    //   resolve: "@medusajs/medusa/caching",
-    //   options: {
-    //     providers: [
-    //       {
-    //         resolve: "@medusajs/caching-redis",
-    //         id: "caching-redis",
-    //         is_default: true,
-    //         options: {
-    //           redisUrl: process.env.CACHE_REDIS_URL,
-    //         },
-    //       },
-    //     ],
-    //   },
-    // },
-    // {
-    //   resolve: "@medusajs/medusa/event-bus-redis",
-    //   options: {
-    //     redisUrl: process.env.REDIS_URL,
-    //   },
-    // },
-    // {
-    //   resolve: "@medusajs/medusa/workflow-engine-redis",
-    //   options: {
-    //     redis: {
-    //       url: process.env.REDIS_URL,
-    //     },
-    //   },
-    // },
-    // {
-    //   resolve: "@medusajs/medusa/locking",
-    //   options: {
-    //     providers: [
-    //       {
-    //         resolve: "@medusajs/medusa/locking-redis",
-    //         id: "locking-redis",
-    //         is_default: true,
-    //         options: {
-    //           redisUrl: process.env.LOCKING_REDIS_URL,
-    //         },
-    //       },
-    //     ],
-    //   },
-    // },
+    {
+      resolve: "@medusajs/medusa/caching",
+      options: {
+        providers: [
+          {
+            resolve: "@medusajs/caching-redis",
+            id: "caching-redis",
+            is_default: true,
+            options: {
+              redisUrl: process.env.REDIS_URL,
+            },
+          },
+        ],
+      },
+    },
+    {
+      resolve: "@medusajs/medusa/event-bus-redis",
+      options: {
+        redisUrl: process.env.REDIS_URL,
+      },
+    },
+    {
+      resolve: "@medusajs/medusa/workflow-engine-redis",
+      options: {
+        redis: {
+          url: process.env.REDIS_URL,
+        },
+      },
+    },
+    {
+      resolve: "@medusajs/medusa/locking",
+      options: {
+        providers: [
+          {
+            resolve: "@medusajs/medusa/locking-redis",
+            id: "locking-redis",
+            is_default: true,
+            options: {
+              redisUrl: process.env.REDIS_URL,
+            },
+          },
+        ],
+      },
+    },
   ],
   plugins: [
     {
