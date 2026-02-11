d21ac57e33c02c3ef9d43a47a804e4036c0cae43
Chadwick <hello@chadwickmacmillan.com>
2026-02-10 21:29:53 -0500

    tax updates

--- src/api/admin/tax_codes/route.ts
+++ src/api/admin/tax_codes/route.ts
@@ -1,8 +1,9 @@
 import { MedusaRequest, MedusaResponse } from "@medusajs/framework/http";
 import TaxCodeService from "../../../modules/tax_code/service";
+import { TAX_CODE_SERVICE } from "../../../modules/tax_code";
 
 export async function GET(req: MedusaRequest, res: MedusaResponse) {
-  const service = req.scope.resolve("Taxcode") as TaxCodeService;
+  const service = req.scope.resolve(TAX_CODE_SERVICE) as TaxCodeService;
 
   const data = await service.listTaxCodes();
   res.json(data);
